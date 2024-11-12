bf160fb4fd0618dfd79d1c763cc633d5064ca6dd
oh <oliver.haufe@sos-berlin.com>
2024-11-12 17:02:56 +0100

    JS-2134

--- src/main/resources/com/sos/inventory/model/instruction/Instruction.java
+++ src/main/resources/com/sos/inventory/model/instruction/Instruction.java
@@ -58,7 +58,8 @@ import com.sos.inventory.model.common.ClassHelper;
         @JsonSubTypes.Type(value = Cycle.class, name = "Cycle"),
         @JsonSubTypes.Type(value = StickySubagent.class, name = "StickySubagent"),
         @JsonSubTypes.Type(value = Options.class, name = "Options"),
-        @JsonSubTypes.Type(value = Break.class, name = "Break")})
+        @JsonSubTypes.Type(value = Break.class, name = "Break"),
+        @JsonSubTypes.Type(value = CaseWhen.class, name = "CaseWhen")})
 public abstract class Instruction
     extends ClassHelper
 {
